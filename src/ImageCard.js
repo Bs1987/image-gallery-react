a94c439c3189b3651f83ed0bce33ef907677e617
Bs1987 <boysax@gmail.com>
2021-05-21 13:43:58 +0200

    Pageheading component

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,9 +1,9 @@
 function ImageCard(props) {
     return (
-        <>
+        <div>
             <h2>{props.subject}</h2>
             <img src={`https://source.unsplash.com/1600x900/?${props.subject}`} alt={props.subject}/>
-        </>
+        </div>
     );
 }
 

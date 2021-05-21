8d647c3087c69e09711ece8cbee0adfcbd1d4c39
Bs1987 <boysax@gmail.com>
2021-05-21 13:07:28 +0200

    Use props in the component

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -3,7 +3,7 @@ function ImageCard(props) {
     return (
         <>
             <h2>{props.subject}</h2>
-            <img src={`https://source.unsplash.com/1600x900/?${props.subject}`}/>
+            <img src={`https://source.unsplash.com/1600x900/?${props.subject}`} alt={props.subject}/>
         </>
     );
 }

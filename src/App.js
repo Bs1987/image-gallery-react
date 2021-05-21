df4d490cf85e97f674fdb8e148f5d4b4038d018c
Bs1987 <boysax@gmail.com>
2021-05-21 12:31:05 +0200

    Make todolist to make components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Beginscherm van applicatie
+// Het is JavaScript, maar sommige delen lijken op HTML syntax
+// JSX (JavaScript dialect, combinatie JavaScript en HTML)
+
+//Hoe maak je een component
+// 1. Begin de HTML gewoon uit te schrijven (maak eerst wat je wil)
+// 2. Als je tevreden bent met je pagina kun je een component refactoren/ opschonen
+// - [] Kopieer een component met alle data erin
+// - [] Zet het in een ander JavaScript bestandje
+// - [] Maak een component functie
+// - [] Die component functie returned JSX
+// - [] Importeer je component functie in APP (of waar je hem wil gebruiken)
+// - [] Gebruik component functie met JSX syntax
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>Nature</h2>
+            <img src="
+https://source.unsplash.com/1600x900/?nature,water" alt=""/>
+
+            <h2>Pasta</h2>
+            <img src="
+https://source.unsplash.com/1600x900/?pasta" alt=""/>
+
+            <h2>Travel</h2>
+            <img src="
+https://source.unsplash.com/1600x900/?travel" alt=""/>
+
+            <h2>Cats</h2>
+            <img src="
+https://source.unsplash.com/1600x900/?cat" alt=""/>
+
+
+
+        </div>
+    );
 }
+
 
 export default App;
